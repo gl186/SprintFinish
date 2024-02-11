bb0edfb4f375ccf75f0c55e242269eb4f3a35331
gl186 <gl186@leicester.ac.uk>
2024-02-11 20:40:15 +0000

    combined module 1 and 2 into main and combined end points so ensemble and ref seq transcript enpoints defined separately within main script

--- ProjectScripts/Main.py
+++ ProjectScripts/Main.py
@@ -8,51 +8,62 @@ from flask import Flask
 from flask_restx import Api, Resource
 import requests
 
-# Define the application as a Flask app with the name defined by __name__ (i.e. the name of the current module)
-# Most tutorials define application as "app"
+# Define the Flask application
 application = Flask(__name__)
+api = Api(application)
 
-# Define the API as api ie. create the RESTful API
-api = Api(app=application)
-
-# Define a name-space to be read Swagger UI which is built into Flask-RESTX
-# The first variable is the path of the namespace and the second variable describes the space
-
-# implemented the Swagger UI and made the shape of API: Linda
-
+# Define the namespace for Ensemble Transcript
 ensembleTranscriptNameSpace = api.namespace('ensemble-transcript',
                                            description='Return a genomic HGVS transcript')
 
-#Define the endpoint which is the ensemble transcript associated with name class. Define the get command to handle HTTP get request
 @ensembleTranscriptNameSpace.route("/<string:ensembleTranscript>")
 class NameClass(Resource):
     def get(self, ensembleTranscript):
-        # TODO: Add VariantRecorder mapping, just a placeholder
+        # Placeholder response
         return {
             "genome": ensembleTranscript
         }
 
+# Define the namespace for RefSeqTranscript
+rst_space = api.namespace('RefSeqTranscript', description='Return a genomic HGVS transcript and genome coordinate')
 
-# The second variable:
-# Allows app to be run in debug mode
+@rst_space.route("/variantvalidator/<string:genome_build>/<string:variant_description>/<string:select_transcripts>")
+class RefSeqTranscriptClass(Resource):
+    def get(self, genome_build, variant_description, select_transcripts):
+        # Make a request to the current variantvalidator rest-api
+        url = f'https://rest.variantvalidator.org/variantvalidator/{genome_build}/{variant_description}/{select_transcripts}'
+        validation = requests.get(url)
+        content = validation.json()
+        return content
+
+# Allow app to be run in debug mode
 if __name__ == '__main__':
-    application.debug = True  # Enable debugging mode
-    application.run(host="127.0.0.1", port=5000)  # Specify a host and port for the app
+    application.debug = True
+    application.run(host="127.0.0.1", port=5000)
 
-# Testing the REST interface defined above utilizing the requests library. This will make HTTP requests to our API endpoints. Georgia
-
-# Define base URL of the API
+# Test Ensemble Transcript API
 BASE_URL = 'http://127.0.0.1:5000'
-
-# Provide example ensembl transcript to test. This can be changed to test different transcripts
 ensemble_transcript = 'ENST00000269305'
+ensemble_transcript_endpoint = f'{BASE_URL}/ensemble-transcript/{ensemble_transcript}'
+ensemble_transcript_response = requests.get(ensemble_transcript_endpoint)
+print("Ensemble Transcript API Response Content:")
+print(ensemble_transcript_response.content.decode('utf-8'))
+if ensemble_transcript_response.status_code == 200:
+    print("Ensemble Transcript API Request successful!")
+else:
+    print("Ensemble Transcript API Request failed with status code:", ensemble_transcript_response.status_code)
 
-# Define the endpoint URL using base URL and an example transcript
-endpoint_url = f'{BASE_URL}/ensemble-transcript/{ensemble_transcript}'
+# Test RefSeqTranscript API
+genome_build = 'GRCh38'
+variant_description = 'NM_001123.3:c.345G>T'
+select_transcripts = 'NM_001123.3'
+refseq_transcript_endpoint = f'{BASE_URL}/RefSeqTranscript/variantvalidator/{genome_build}/{variant_description}/{select_transcripts}'
+refseq_transcript_response = requests.get(refseq_transcript_endpoint)
+print("\nRefSeqTranscript API Response Content:")
+print(refseq_transcript_response.content.decode('utf-8'))
+if refseq_transcript_response.status_code == 200:
+    print("RefSeqTranscript API Request successful!")
+else:
+    print("RefSeqTranscript API Request failed with status code:", refseq_transcript_response.status_code)
 
-# Send GET request to the endpoint
-response = requests.get(endpoint_url)
 
-# Print the response status code and content
-print('Response Status Code:', response.status_code)
-print('Response Content:', response.json())
