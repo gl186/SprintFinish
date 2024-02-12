d57142b0eb7fbf9be1e0d5a8e953e3866a07d0a9
tophie97 <christophgjohnson@hotmail.com>
2024-02-12 13:18:18 -0500

    Added new Module 3 and added VEP Module 4. Debugged both versions where needed. Better Script organization.

--- ProjectScripts/Main.py
+++ ProjectScripts/Main.py
@@ -25,60 +25,25 @@ class NameClass(Resource):
         }
 
 # Define the namespace for RefSeqTranscript - Georgia and Christoph
-rst_space = api.namespace('RefSeqTranscript', description='Return a genomic HGVS transcript and genome coordinate')
+rst_space = api.namespace('RefSeq Release Version 222', description = 'Return a genomic HGVS transcript and genome coordinate')
 
 @rst_space.route("/variantvalidator/<string:genome_build>/<string:variant_description>/<string:select_transcripts>")
 class RefSeqTranscriptClass(Resource):
     def get(self, genome_build, variant_description, select_transcripts):
-        # Make a request to the current variantvalidator rest-api
-        url = f'https://rest.variantvalidator.org/variantvalidator/{genome_build}/{variant_description}/{select_transcripts}'
-        validation = requests.get(url)
-        content = validation.json()
+
+        #Make a request to the current variantvalidator rest-api
+        url ='/'.join(["https://rest.variantvalidator.org/variantvalidator,genome_build, variant_description, select_trasncripts"])
+        validation = request.get(url)
+        content =validation.json()
         return content
 
-# Allow app to be run in debug mode
+
+# allows app to be run in debug mode
 if __name__ == '__main__':
-    application.debug = True
-    application.run(host="127.0.0.1", port=5000)
-
-# Test Ensemble Transcript API - Georgia
-BASE_URL = 'http://127.0.0.1:5000'
-ensemble_transcript = 'ENST00000269305'
-ensemble_transcript_endpoint = f'{BASE_URL}/ensemble-transcript/{ensemble_transcript}'
-ensemble_transcript_response = requests.get(ensemble_transcript_endpoint)
-print("Ensemble Transcript API Response Content:")
-print(ensemble_transcript_response.content.decode('utf-8'))
-if ensemble_transcript_response.status_code == 200:
-    print("Ensemble Transcript API Request successful!")
-else:
-    print("Ensemble Transcript API Request failed with status code:", ensemble_transcript_response.status_code)
-
-# Test RefSeqTranscript API - Georgia
-genome_build = 'GRCh38'
-variant_description = 'NM_001123.3:c.345G>T'
-select_transcripts = 'NM_001123.3'
-refseq_transcript_endpoint = f'{BASE_URL}/RefSeqTranscript/variantvalidator/{genome_build}/{variant_description}/{select_transcripts}'
-refseq_transcript_response = requests.get(refseq_transcript_endpoint)
-print("\nRefSeqTranscript API Response Content:")
-print(refseq_transcript_response.content.decode('utf-8'))
-if refseq_transcript_response.status_code == 200:
-    print("RefSeqTranscript API Request successful!")
-else:
-    print("RefSeqTranscript API Request failed with status code:", refseq_transcript_response.status_code)
-
+    application.debug = True #enable debugging mode
+    application.run(host="127.0.0.1", port=5000) # specify a host and port for the app
 
 #Merged Module 3 Variant Variant Validator into Main.py - Christoph and Georgia
-# Import modules
-from flask import Flask, make_response
-from flask_restx import Api, Resource, fields, reqparse
-import requests
-
-# Define the application as a Flask app with the name defined by __name__
-app = Flask(__name__)
-
-# Define the Api as api
-api = Api(app, title="LOVD Variant API", description="Retrieve variant data from LOVD")
-
 # Request parser to identify specific content-type requests
 parser = reqparse.RequestParser()
 parser.add_argument('content-type', type=str, help='Accepted\n-application/json')
@@ -116,18 +81,6 @@ if __name__ == "__main__":
 
 
 #Added VEP Module 4 support - Christoph
-# Import modules
-from flask import Flask
-from flask_restx import Api, Resource, reqparse
-import requests
-
-# Define the application as a Flask app with the name defined by __name__
-app = Flask(__name__)
-
-# Define the Api as api
-api = Api(app, version='1.0', title='Variant Validator_VEP API',
-          description='Retrieve variant data and Ensembl VEP annotations')
-
 # Request parser to identify specific content-type requests
 parser = reqparse.RequestParser()
 parser.add_argument('content-type', type=str, help='Accepted\n-application/json')
@@ -172,3 +125,28 @@ class VariantVEPResource(Resource):
 if __name__ == "__main__":
     app.run(debug=True)
     app.run(host="127.0.0.1", port=5000)  # Specify a host and port for the app
+
+# Test Ensemble Transcript API - Georgia
+BASE_URL = 'http://127.0.0.1:5000'
+ensemble_transcript = 'ENST00000269305'
+ensemble_transcript_endpoint = f'{BASE_URL}/ensemble-transcript/{ensemble_transcript}'
+ensemble_transcript_response = requests.get(ensemble_transcript_endpoint)
+print("Ensemble Transcript API Response Content:")
+print(ensemble_transcript_response.content.decode('utf-8'))
+if ensemble_transcript_response.status_code == 200:
+    print("Ensemble Transcript API Request successful!")
+else:
+    print("Ensemble Transcript API Request failed with status code:", ensemble_transcript_response.status_code)
+
+# Test RefSeqTranscript API - Georgia
+genome_build = 'GRCh38'
+variant_description = 'NM_001123.3:c.345G>T'
+select_transcripts = 'NM_001123.3'
+refseq_transcript_endpoint = f'{BASE_URL}/RefSeqTranscript/variantvalidator/{genome_build}/{variant_description}/{select_transcripts}'
+refseq_transcript_response = requests.get(refseq_transcript_endpoint)
+print("\nRefSeqTranscript API Response Content:")
+print(refseq_transcript_response.content.decode('utf-8'))
+if refseq_transcript_response.status_code == 200:
+    print("RefSeqTranscript API Request successful!")
+else:
+    print("RefSeqTranscript API Request failed with status code:", refseq_transcript_response.status_code)
