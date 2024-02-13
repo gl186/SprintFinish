6faa89dd8217af324809687098be173c473cfd5a
gl186 <139965213+gl186@users.noreply.github.com>
2024-02-13 15:39:12 +0000

    Update Main.py
    summary of group discussion not to use this code

--- ProjectScripts/Main.py
+++ ProjectScripts/Main.py
@@ -1,3 +1,5 @@
+#code reviewed in team meeting. this code is not to be used. conflict with main branch due to incorrect push/pull reqyest. GL and CT copying code over onto main. 
+
 from flask import Flask, request
 from flask_restx import Api, Resource, reqparse
 import requests
