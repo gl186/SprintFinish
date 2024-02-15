ec092f12239db8a2020c56ccba2f5e9c595d4d15
gl186 <gl186@leicester.ac.uk>
2024-02-15 12:37:38 +0000

    trialling main.py coding

--- ProjectScripts/Main.py
+++ ProjectScripts/Main.py
@@ -3,3 +3,10 @@ from flask_restx import Api, Resource, reqparse
 import requests
 import logging
 import logging.handlers as handlers
+
+#import module 1
+from Modules import module1_variantrecoder
+from Modules.module1_variantrecoder import get
+
+
+
