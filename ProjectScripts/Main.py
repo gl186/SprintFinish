e1405917e4d2cbdf27d4f47e74878b75cc6deb37
gl186 <gl186@leicester.ac.uk>
2024-02-15 13:03:35 +0000

    trialling main.py coding, calling class attributes not working with API yet

--- ProjectScripts/Main.py
+++ ProjectScripts/Main.py
@@ -6,18 +6,29 @@ import logging.handlers as handlers
 
 #import module 1 Var Recoder
 from Modules import module1_variantrecoder
+#need to call relevant attribute or class item from module code. tried all items and wont accept. repeated error message "no attribute"
+    #print all available attributes or classes from module1 with:
+        #print(dir(module1_variantrecoder))
+module1_variantrecoder.__name__
 
 #import module 2 VarVal
 from Modules import module2_variantvalidator
+#print(dir(module2_variantvalidator))
+module2_variantvalidator.__name__
 
 #import module 3 LOVD
 from Modules import module3_VV_LOVD
+module3_VV_LOVD.__name__
 
 #import module 4 VEP
 from Modules import module4_VV_VEP
+module4_VV_VEP.__name__
 
 #import module 5 SPDI
 from Modules import module5_SPDI
+module5_SPDI.__name__
+
+
 
 
 
