ad633dfd8adcc30d026a4aea9879d459815d2efb
gl186 <gl186@leicester.ac.uk>
2024-02-15 12:40:50 +0000

    trialling main.py coding

--- ProjectScripts/Main.py
+++ ProjectScripts/Main.py
@@ -4,9 +4,20 @@ import requests
 import logging
 import logging.handlers as handlers
 
-#import module 1
+#import module 1 Var Recoder
 from Modules import module1_variantrecoder
-from Modules.module1_variantrecoder import get
+
+#import module 2 VarVal
+from Modules import module2_variantvalidator
+
+#import module 3 LOVD
+from Modules import module3_VV_LOVD
+
+#import module 4 VEP
+from Modules import module4_VV_VEP
+
+#import module 5 SPDI
+from Modules import module5_SPDI
 
 
 
