1c311d1f1a55d2368b928e1cbbaebdef00b30622
tophie97 <christophgjohnson@hotmail.com>
2024-02-16 10:52:37 -0500

    Removed logger from module

--- ProjectScripts/Modules/module2_variantvalidator/module2_variantvalidator.py
+++ ProjectScripts/Modules/module2_variantvalidator/module2_variantvalidator.py
@@ -4,23 +4,6 @@ from flask_restx import Api, Resource
 import requests
 import logging
 
-# Determine log file format and create log file
-LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
-logging.basicConfig(filename = "E:\\python\\module2.log",
-                    level = logging.DEBUG,
-                    format = LOG_FORMAT)
-logger = logging.getLogger()
-
-# Testing logger messages
-logger.info("This is an info message")
-logging.debug("This is a debug message")
-logging.warning("This is a warning message")
-logging.error("This is an error message")
-logging.critical("This is a critical message")
-
-# Allow the level to be printed in the logger
-print(logger.level)
-
 # Define the application as a Flask app with the name defined by __name__ (i.e. the name of the current module)
 application = Flask(__name__)
 
