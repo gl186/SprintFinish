b915bd05ba4671a51dec6dc8d88b9232dffb4d27
tophie97 <christophgjohnson@hotmail.com>
2024-02-16 09:43:42 -0500

    Added stable module2 logger

--- ProjectScripts/Modules/module2_variantvalidator/module2_variantvalidator.py
+++ ProjectScripts/Modules/module2_variantvalidator/module2_variantvalidator.py
@@ -4,19 +4,21 @@ from flask_restx import Api, Resource
 import requests
 import logging
 
+# Determine log file format and create log file
 LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
 logging.basicConfig(filename = "E:\\python\\module2.log",
                     level = logging.DEBUG,
                     format = LOG_FORMAT)
 logger = logging.getLogger()
 
-#Testing logger messages
+# Testing logger messages
 logger.info("This is an info message")
 logging.debug("This is a debug message")
 logging.warning("This is a warning message")
 logging.error("This is an error message")
 logging.critical("This is a critical message")
 
+# Allow the level to be printed in the logger
 print(logger.level)
 
 # Define the application as a Flask app with the name defined by __name__ (i.e. the name of the current module)
@@ -42,6 +44,6 @@ class RefSeqTranscriptClass(Resource):
         return content
 
 
-# allows app to be run in debug mode
+# Allows app to be run in debug mode
 if __name__ == '__main__':
     application.debug = True #enable debugging mode
