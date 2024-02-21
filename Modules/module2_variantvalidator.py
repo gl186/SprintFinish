dbbb7e3722b5ef68ae3b58f49324c57ba5d9219a
LMP2005 <153301113+LMP2005@users.noreply.github.com>
2024-02-21 16:25:04 +0000

    Update module2_variantvalidator.py

--- Modules/module2_variantvalidator.py
+++ Modules/module2_variantvalidator.py
@@ -25,14 +25,14 @@ def get_genomic_info_from_transcript(transcript_id):
         print(f"Error: {e}")
         return None, None
 
-def test_return_hgvs():
-    return "NC_000017.10:g.48275363C>A"
+#def test_return_hgvs():
+    #return "NC_000017.10:g.48275363C>A"
 
 # Example usage
-#transcript_id = "NM_000088.4"   # Replace with your RefSeq transcript ID
-#genomic_hgvs, genomic_coordinates = get_genomic_info_from_transcript(transcript_id)
+transcript_id = "NM_000088.4"   # Replace with your RefSeq transcript ID
+genomic_hgvs, genomic_coordinates = get_genomic_info_from_transcript(transcript_id)
 
-# if genomic_hgvs and genomic_coordinates:
-#     print(f"Genomic HGVS: {genomic_hgvs}")
-#     print(f"Genomic Coordinates: {genomic_coordinates}")
+if genomic_hgvs and genomic_coordinates:
+     print(f"Genomic HGVS: {genomic_hgvs}")
+     print(f"Genomic Coordinates: {genomic_coordinates}")
 
